91fa0fa091cd968a666ae049aa9ff0da7bda3514
zozibush <zozibush88@google.com>
2023-12-20 22:54:05 +0900

    Added basic description for point2d.hpp

---
+++ include/geometry/point2d.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file geometry/point2d.hpp
+ * @author zozibush (zozibush88@gmail.com)
+ * @brief Point class declaration with 2-dimesion
+ * @version 1.0.0
+ * @date 2023-12-20
+ * @copyright Copyright (c) 2023 zozibush, All Rights Reserved.
+ */
+
+ // Copyright (c) 2023 zozibush, All RIghts Reserved
+ // Authors: zozibush
+
+#ifndef ZOZIBUSH__GEOMETRY_POINT_2_HPP_
+#define ZOZIBUSH__GEOMETRY_POINT_2_HPP_
+
+
+#endif
