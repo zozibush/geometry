5c90e8b6537dd37e97042db3933dd78e04f576e8
zozibush <zozibush88@google.com>
2023-12-21 00:16:23 +0900

    Added documetation with doxygen

---
+++ documents/html/navtreedata.js
@@ -0,0 +1,14 @@
+var NAVTREE =
+[
+  [ "geometry", "index.html", [
+    [ "geometry", "md__r_e_a_d_m_e.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ documents/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['geometry',['geometry',['../md__r_e_a_d_m_e.html',1,'']]]
+];
