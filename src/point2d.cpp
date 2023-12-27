7084cd72e52d3140d795ede4f72460622ed31c6a
zozibush <zozibush88@google.com>
2023-12-27 21:45:12 +0900

    Added implementation about operatior add in point2d

--- src/point2d.cpp
+++ src/point2d.cpp
@@ -19,10 +19,15 @@ auto Point2D::CalculateDistance(const Point2D& lhs,
 }
 
 auto Point2D::GetX() const -> double { return x_; }
-
 auto Point2D::GetY() const -> double { return y_; }
 
 auto Point2D::SetX(double input_x) -> void { x_ = input_x; }
 auto Point2D::SetY(double input_y) -> void { y_ = input_y; }
+
+auto Point2D::operator+(const Point2D& rhs) const -> Point2D{
+  auto kX=this->GetX()+rhs.GetX();
+  auto kY=this->GetY()+rhs.GetY();
+  return Point2D(kX,kY);
+}
 
 }  // namespace zozibush::geometry
