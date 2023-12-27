a896c2fb1988f581f7c4f6f780e8955d7c550099
zozibush <zozibush88@google.com>
2023-12-27 22:01:34 +0900

    Added test about operator add+accumulate between point2d

--- test/unit/point2d.cpp
+++ test/unit/point2d.cpp
@@ -125,4 +125,20 @@ TEST(GeometryPoint2D, OperatorSubtract) {
     EXPECT_FLOAT_EQ(kSourceY - kTargetY, result.GetY());
   }
 }
+TEST(GeometryPoint2D, OperatorAddAccumulate) {
+  for (uint32_t i = 0; i < kTestCount; ++i) {
+    const auto kSourceX = static_cast<double>(std::rand());
+    const auto kSourceY = static_cast<double>(std::rand());
+    const auto kTargetX = static_cast<double>(std::rand());
+    const auto kTargetY = static_cast<double>(std::rand());
+
+    Point2D source(kSourceX, kSourceY);
+    Point2D target(kTargetX, kTargetY);
+
+    source += target;
+
+    EXPECT_FLOAT_EQ(kSourceX + kTargetX, source.GetX());
+    EXPECT_FLOAT_EQ(kSourceY + kTargetY, source.GetY());
+  }
+}
 }  // namespace zozibush::geometry
