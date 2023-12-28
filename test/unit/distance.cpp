52091f220930ee829426cdf052da92424242678a
zozibush <zozibush88@google.com>
2023-12-28 19:10:59 +0900

    Added distance class assignment test

--- test/unit/distance.cpp
+++ test/unit/distance.cpp
@@ -10,4 +10,9 @@ TEST(GeometryDistance, Constructor) {
   Distance distance2(distance1);
   Distance distance3(std::move(Distance()));
 }
+TEST(GeometryDistance, Assignment) {
+  Distance distance1;
+  auto distance2 = distance1;
+  auto distance3 = std::move(Distance());
+}
 }  // namespace zozibush::geometry
