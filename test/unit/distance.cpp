8638e46f812f0048479dc89cb31bf1b689dc7f50
zozibush <zozibush88@google.com>
2023-12-28 20:07:40 +0900

    Added distance class constructor with input value test

--- test/unit/distance.cpp
+++ test/unit/distance.cpp
@@ -4,11 +4,32 @@
 
 #include "gtest/gtest.h"
 
+namespace {
+constexpr uint32_t kTestCount = 1000U;
+}
+
 namespace zozibush::geometry {
 TEST(GeometryDistance, Constructor) {
   Distance distance1;
   Distance distance2(distance1);
   Distance distance3(std::move(Distance()));
+}
+TEST(GeometryDistance, ConstructorWithInputValue) {
+  for (uint32_t i = 0; i < kTestCount; ++i) {
+    Distance distance1(static_cast<double>(std::rand()));
+    Distance distance2(static_cast<double>(std::rand()),
+                       Distance::Type::kKilometer);
+    Distance distance3(static_cast<double>(std::rand()),
+                       Distance::Type::kMeter);
+    Distance distance4(static_cast<double>(std::rand()),
+                       Distance::Type::kCentimeter);
+    Distance distance5(static_cast<double>(std::rand()),
+                       Distance::Type::kMillimeter);
+    Distance distance6(static_cast<double>(std::rand()),
+                       Distance::Type::kMicrometer);
+    Distance distance7(static_cast<double>(std::rand()),
+                       Distance::Type::kNanometer);
+  }
 }
 TEST(GeometryDistance, Assignment) {
   Distance distance1;
