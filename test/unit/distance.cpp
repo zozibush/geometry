a63464085d083a72a329c9fab12c15a69fe8d5a8
zozibush <zozibush88@google.com>
2023-12-28 21:07:44 +0900

    Added distance class operator not equal test

--- test/unit/distance.cpp
+++ test/unit/distance.cpp
@@ -98,4 +98,14 @@ TEST(GeometryDistance, OperatorEqual) {
   EXPECT_TRUE(distance_by_kilo == distance_by_nano);
   EXPECT_TRUE(distance == distance_by_nano);
 }
+TEST(GeometryDistance, OperatorNotEqual) {
+  const double kInputValue = static_cast<double>(2038.0);
+  Distance distance_by_kilo(kInputValue, Distance::Type::kKilometer);
+  Distance distance(kInputValue * 1.0e+3, Distance::Type::kMeter);
+  Distance distance_by_nano(kInputValue * 1.0e+12, Distance::Type::kNanometer);
+
+  EXPECT_FALSE(distance_by_kilo != distance);
+  EXPECT_FALSE(distance_by_kilo != distance_by_nano);
+  EXPECT_FALSE(distance != distance_by_nano);
+}
 }  // namespace zozibush::geometry
