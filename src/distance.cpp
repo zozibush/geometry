e7edf0a4408842f9550670857153459fd8e7c7d3
zozibush <zozibush88@google.com>
2023-12-28 21:42:46 +0900

    Added distance class operator bigger equal implementation

--- src/distance.cpp
+++ src/distance.cpp
@@ -78,4 +78,7 @@ auto Distance::operator<=(const Distance &other) const -> bool {
 auto Distance::operator>(const Distance &other) const -> bool {
   return (nanometer_ > other.nanometer_);
 }
+auto Distance::operator>=(const Distance &other) const -> bool {
+  return (nanometer_ >= other.nanometer_);
+}
 }  // namespace zozibush::geometry
